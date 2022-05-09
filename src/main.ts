7381877d57d1f4d30ab044ed6815bb9692440395
LOG1997 <2694233102@qq.com>
2022-05-09 15:38:21 +0800

    store

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,12 @@
+/*
+ * @Author: LOG
+ * @FilePath: \vue3default\src\main.ts
+ * @Descripttion: 
+ * @version: 
+ * @Date: 2022-05-02 09:10:02
+ * @LastEditors: LOG
+ * @LastEditTime: 2022-05-09 15:37:51
+ */
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'virtual:windi.css'
@@ -5,4 +14,5 @@ import "./assets/css/reset.css"
 // element-plus
 import 'element-plus/dist/index.css'
 import router from "./router/index";
-createApp(App).use(router).mount('#app')
+import store from "./store/index";
+createApp(App).use(router).use(store).mount('#app')
---
+++ src/store/index.ts
@@ -0,0 +1,22 @@
+/*
+ * @Author: LOG
+ * @FilePath: \vue3default\src\views\index.ts
+ * @Descripttion: 
+ * @version: 
+ * @Date: 2022-05-09 15:35:00
+ * @LastEditors: LOG
+ * @LastEditTime: 2022-05-09 15:35:01
+ */
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+  },
+  mutations: {
+
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
